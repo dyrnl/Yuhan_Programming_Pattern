1008e521eeb5853e2b508565ace291f2fc32f04a
dyrnl <cu8339908@gmail.com>
2024-03-21 10:57:25 +0900

    [ES] 과제제출

---
+++ 3week/3week-1/Lecture03-HW/Lecture03-HW.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+enum ForeColour 
+{
+    Default = 0,
+    Black = 30,
+    Red = 31,
+    Green = 32,
+    Yellow = 33,
+    Blue = 34,
+    Magenta = 35,
+    Cyan = 36,
+    White = 37,
+};
+
+void drawBlock(int colorCode)
+{
+    std::cout << "\x1b[" << colorCode << "m";
+    std::cout << "블록블록블록\n블록블록블록\n블록블록블록" << std::endl;
+    std::cout << "\x1b[" << ForeColour::White << "m";
+}
+int main()
+{
+    /*
+    * 흰색 블록을 그리는 키코드 : 1 (1)
+    * 빨강색 블록을 그리는 키코드 : 2 (10)
+    * 녹색 블록을 그리는 키코드 : 4 (100)
+    * 노란색 블록을 그리는 키코드 : 8 (1000)
+    * 청록색 블록을 그리는 키코드 : 16 (10000)
+    * 마젠타색 블록을 그리는 키코드 : 32 (100000)
+    * 프로그램 종료 : 64 (1000000)
+    *
+    * 프로그램을 시작하면 아래와 같이 쓰시오
+    *
+    *
+    * 화면에 그림을 그리는 프로그램입니다.
+    * 학번 : 349218347(본인학번)
+    * 이름 : 본인이름
+    *
+    * 화면에 그릴 물체코드를 입력하세요 :
+    * (반복)
+    */
+
+    std::cout << "화면에 그림을 그리는 프로그램입니다." << std::endl;
+    std::cout << "학번 : 202327021" << std::endl;
+    std::cout << "이름 : 송영진" << std::endl;
+    
+    int InputNum = 0;
+
+    while (true)
+    {
+        std::cout << "화면에 그릴 물체코드를 입력해주세요 : ";
+        std::cin >> InputNum;
+
+        if (InputNum == 1)
+        {
+            drawBlock(ForeColour::White);
+        }
+        else if (InputNum == 2)
+        {
+            drawBlock(ForeColour::Red);
+        }
+        else if (InputNum == 4)
+        {
+            drawBlock(ForeColour::Green);
+        }
+        else if (InputNum == 8)
+        {
+            drawBlock(ForeColour::Yellow);
+        }
+        else if (InputNum == 16)
+        {
+            drawBlock(ForeColour::Cyan);
+        }
+        else if (InputNum == 32)
+        {
+            drawBlock(ForeColour::Magenta);
+        }
+        else if (InputNum == 64)
+        {
+            std::cout << "프로그램 종료." << std::endl;
+            break;
+        }
+        else
+        {
+            std::cout << "잘못된 값입니다. 다시 입력해주세요." << std::endl;
+        }
+    }
+        return 0;
+}
---
+++ 3week/3week.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+
+
+int main()
+{
+	int num;
+	std::cout << "전 제가 좋습니다";
+	std::cin >> num;
+	std::cout << "근데 송재원은" << num << "번 죽여도 분이 안 풀릴 것 같다";
+
+	int num = 0;
+	std::string name;
+	while (num != 9999)
+	{
+		std::cout << "게임 실행" << std::endl;
+		std::cout << "내가 싫어하는 숫자를 써봐 : ";
+		std::cin >> num >> name;
+		std::cout << "재원아 왜 그러니..?" << std::endl;
+	}
+	std::cout << "그럼 " << num << "대 맞자" << std::endl;
+
+	return 0;
+}
